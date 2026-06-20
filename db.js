36db277743d78a66044612d7d579db8c02f93590
Shivom <shivomyada6269>
2026-06-20 22:12:55 +0530

    fix: restore missing 'c' in const stripped by BOM removal in server.js and db.js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-onst mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 // Connect to MongoDB
 const connectDB = async () => {
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const cors = require('cors');
 const crypto = require('crypto');
 const fs = require('fs');
